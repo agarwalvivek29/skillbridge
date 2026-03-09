dd1ab83888ebf109ac591e9f39a43c0de8ec0def
Vivek Agarwal <agarwalvivek2902@gmail.com>
2026-03-09 17:39:20 +0530

    fix(api): renumber reviews migration from 0008 to 0010
    Three agents each created migration 0008. Renumber to avoid collision:
      0008 = disputes
      0009 = reputation
      0010 = ratings (this)
    
    Update revision to 0010 and down_revision to chain off 0009.

--- services/api/src/migrations/versions/0010_create_reviews.py
+++ services/api/src/migrations/versions/0010_create_reviews.py
@@ -1,7 +1,7 @@
 """create reviews table
 
-Revision ID: 0008
-Revises: 0007
+Revision ID: 0010
+Revises: 0009
 Create Date: 2026-03-09
 
 """
@@ -12,8 +12,8 @@ import sqlalchemy as sa
 from alembic import op
 from sqlalchemy.dialects import postgresql
 
-revision: str = "0008"
-down_revision: Union[str, None] = "0007"
+revision: str = "0010"
+down_revision: Union[str, None] = "0009"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
